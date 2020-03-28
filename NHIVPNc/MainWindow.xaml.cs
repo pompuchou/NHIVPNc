5b7b63c8f898072f05b61ed5278ae3bbeb2d4d26
bestariada <bestariada@gmail.com>
2020-03-28 22:48:58 +0800

    datagrid

--- NHIVPNc/MainWindow.xaml.cs
+++ NHIVPNc/MainWindow.xaml.cs
@@ -12,17 +12,33 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace NHIVPNc
 {
     /// <summary>
-    /// MainWindow.xaml 的互動邏輯
+    /// 2020/03/22 created
+    /// 這是我第一個cs, WPF的程式
+    /// 2020/03/28 什麼是相對於winform的webbrowser
+    /// menustrip=>menu; 
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void VPN_click(object sender, RoutedEventArgs e)
+        {
+            /// 2020/03/28 created, transcribed from vb.net
+            vpnweb.Navigate("https://medvpn.nhi.gov.tw/ieae0000/IEAE0200S01.aspx");
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            NHIDataContext dc = new NHIDataContext();
+            DLData.ItemsSource = dc.tbl_download;
         }
     }
 }
