dfc6b133d0a8906dbd0e8cd563e77593a3cd498d
bestariada <bestariada@gmail.com>
2020-05-05 05:14:17 +0800

    new method

--- NHIVPNc/MainWindow.xaml.cs
+++ NHIVPNc/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using mshtml;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Timers;
 using System.Windows;
@@ -31,9 +32,8 @@ namespace NHIVPNc
 
         private System.Timers.Timer _timer1;
 
-        private int current_page, total_pages, current_line, total_lines;
-        private bool should_click = false;
-        private readonly Queue<int> queue_item = new Queue<int>();
+        private int current_page, total_pages, current_line;
+        private readonly Queue<int> queue_files = new Queue<int>();
 
         #endregion "Declaration"
 
@@ -97,6 +97,11 @@ namespace NHIVPNc
 
         private void Vpn_PageData()
         {
+            vpnweb.LoadCompleted -= Vpn_Page_LoadCompleted;
+            // local variables
+            Dictionary<int, string> VPN_files = new Dictionary<int, string>();
+            List<string> Local_files = new List<string>();
+
             /// 取得gvList
             HTMLDocument d = (HTMLDocument)vpnweb.Document;
             IHTMLElement gvDownLoad = d.getElementById("ContentPlaceHolder1_gvDownLoad");
@@ -106,151 +111,154 @@ namespace NHIVPNc
             /// 只好第一層是ihtmlelement
             IHTMLElementCollection trs_ = gvDownLoad.all;
             IHTMLElementCollection trs = trs_.tags("tr");
-
-            // 使用item這個方法可以將集合中的元素取出
-            // 第一個參數代表的是順序,但是在msdn中標示為name
-            // 第二個參數msdn中標示為index,但經過測試後,指的並不是順序,所以目前無法確定他的用途
-            // 如果有知道的朋友,也請跟我說一下
-            // current_line = 0 是標題行
-
-            // initialization
-            current_line = 1;
-            // total_lines 比較少一條, 因為有標題列
-            total_lines = trs.length - 1;
-
-            Vpn_LineData();
-        }
-
-        private void Vpn_LineData()
-        {
-            vpnweb.LoadCompleted -= Vpn_Page_LoadCompleted;
-            vpnweb.LoadCompleted -= Vpn_Line_LoadCompleted;
-
-            if (should_click)
-            {
-                System.Threading.Thread.Sleep(1000);
-
-                System.Threading.ThreadStart th_begin = new System.Threading.ThreadStart(Work_todo);
-                System.Threading.Thread thr = new System.Threading.Thread(th_begin)
-                {
-                    IsBackground = true,
-                    Name = "PressS"
-                };
-                thr.Start();
-                should_click = false;
-            }
-
-            /// 取得gvList
-            HTMLDocument d = (HTMLDocument)vpnweb.Document;
-            IHTMLElement gvDownLoad = d.getElementById("ContentPlaceHolder1_gvDownLoad");
             DateTime current_time = DateTime.Now;
 
-            /// 讀取
-            /// 20200503 我發現Html Agility Pack不能click
-            /// 只好第一層是ihtmlelement
-
-            #region "Write"
-
-            NHIDataContext dc = new NHIDataContext();
-            IHTMLElementCollection trs_ = gvDownLoad.all;
-            IHTMLElementCollection trs = trs_.tags("tr");
-
             // 使用item這個方法可以將集合中的元素取出
             // 第一個參數代表的是順序,但是在msdn中標示為name
             // 第二個參數msdn中標示為index,但經過測試後,指的並不是順序,所以目前無法確定他的用途
             // 如果有知道的朋友,也請跟我說一下
+
             // current_line = 0 是標題行
+            /// 20200503 我發現Html Agility Pack不能click
+            /// 只好第一層是ihtmlelement
 
-            IHTMLElement tr = trs.item(current_line, null);
-            HtmlDocument h_ = new HtmlDocument();
-            h_.LoadHtml(tr.innerHTML);
-            HtmlNodeCollection tds = h_.DocumentNode.SelectNodes("//td");
+            #region write in sql table
 
-            short order_n = 0;
-            string s_f_name = string.Empty, s_f_remark = string.Empty, s_remark = string.Empty;
-            DateTime d_SDATE = DateTime.Now;
-            bool b_archive = false;
-
-            foreach (HtmlNode td in tds)
+            VPN_files.Clear();
+            for (current_line = 1; current_line < trs.length; current_line++)
             {
-                switch (order_n)
+                IHTMLElement tr = trs.item(current_line, null);
+                HtmlDocument h_ = new HtmlDocument();
+                h_.LoadHtml(tr.innerHTML);
+                HtmlNodeCollection tds = h_.DocumentNode.SelectNodes("//td");
+
+                short order_n = 0;
+                string s_f_name = string.Empty, s_f_remark = string.Empty, s_remark = string.Empty;
+                DateTime d_SDATE = DateTime.Now;
+                bool b_archive = false;
+
+                foreach (HtmlNode td in tds)
                 {
-                    case 0:  //檔案名稱
-                        s_f_name = td.InnerText;
-                        break;
+                    switch (order_n)
+                    {
+                        case 0:  //檔案名稱
+                            s_f_name = td.InnerText;
+                            VPN_files.Add(current_line, s_f_name);
+                            break;
 
-                    case 1: //檔案說明
-                        s_f_remark = td.InnerText;
-                        break;
+                        case 1: //檔案說明
+                            s_f_remark = td.InnerText;
+                            break;
 
-                    case 2: //下載備註
-                        s_remark = td.InnerText;
-                        break;
+                        case 2: //下載備註
+                            s_remark = td.InnerText;
+                            break;
 
-                    case 3: //提供下載日期
-                        if (td.InnerText != string.Empty)
+                        case 3: //提供下載日期
+                            if (td.InnerText != string.Empty)
+                            {
+                                HtmlNode div = td.ChildNodes[1]; // 怪,第一個member竟然是/r/n
+                                string[] temp_s = div.InnerHtml.Replace("<br>", "|").Split('|');
+                                string[] temp_d = temp_s[0].Split('/');
+                                d_SDATE = DateTime.Parse($"{int.Parse(temp_d[0]) + 1911}/{temp_d[1]}/{temp_d[2]} {temp_s[1]}");
+                            }
+                            break;
+
+                        case 4: //檔案下載
+                            if (td.SelectNodes("//a").Count == 2)
+                            {
+                                b_archive = false;
+                            }
+                            else
+                            {
+                                b_archive = true;
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+                    order_n++;
+                }
+
+                using (NHIDataContext dc = new NHIDataContext())
+                {
+                    var q = from p in dc.tbl_download
+                            where (p.f_name == s_f_name) && (p.SDATE == d_SDATE)
+                            select p;
+                    if (q.Count() == 0)
+                    {
+                        tbl_download newNHI = new tbl_download()
                         {
-                            HtmlNode div = td.ChildNodes[1]; // 怪,第一個member竟然是/r/n
-                            string[] temp_s = div.InnerHtml.Replace("<br>", "|").Split('|');
-                            string[] temp_d = temp_s[0].Split('/');
-                            d_SDATE = DateTime.Parse($"{int.Parse(temp_d[0]) + 1911}/{temp_d[1]}/{temp_d[2]} {temp_s[1]}");
-                        }
-                        break;
-
-                    case 4: //檔案下載
-                        if (td.SelectNodes("//a").Count == 2)
+                            QDATE = current_time,
+                            f_name = s_f_name,
+                            f_remark = s_f_remark,
+                            remark = s_remark,
+                            SDATE = d_SDATE
+                        };
+                        //存檔
+                        if (b_archive)
                         {
-                            b_archive = false;
+                            newNHI.download = false;
                         }
                         else
                         {
-                            b_archive = true;
+                            newNHI.download = true;
                         }
-                        break;
-
-                    default:
-                        break;
+                        dc.tbl_download.InsertOnSubmit(newNHI);
+                        dc.SubmitChanges();
+                    }
                 }
-                order_n++;
             }
 
-            var q = from p in dc.tbl_download
-                    where (p.f_name == s_f_name) && (p.SDATE == d_SDATE)
-                    select p;
-            //if (q.Count() == 0)
-            //{
-            tbl_download newNHI = new tbl_download()
+            #endregion write in sql table
+
+            #region download files
+
+            Local_files.Clear();
+
+            // read file data from directory
+            foreach (string f in Directory.GetFiles(@"D:\IDrive-Sync\archive"))
             {
-                QDATE = current_time,
-                f_name = s_f_name,
-                f_remark = s_f_remark,
-                remark = s_remark,
-                SDATE = d_SDATE
+                Local_files.Add(f.Replace(@"D:\IDrive-Sync\archive\", "").Replace(".zip", ""));
+            }
+
+            // making queue_files, using linq
+            foreach (KeyValuePair<int, string> v in VPN_files)
+            {
+                if (!Local_files.Contains(v.Value)) queue_files.Enqueue(v.Key);
+            }
+
+            // execution
+            this._timer1 = new System.Timers.Timer
+            {
+                Interval = 6000
             };
-            //存檔
-            if (b_archive)
-            {
-                newNHI.download = false;
-            }
-            else
-            {
-                should_click = true;
-                newNHI.download = true;
-            }
-            //dc.tbl_download.InsertOnSubmit(newNHI);
-            //dc.SubmitChanges();
-            //}
+            this._timer1.Elapsed += new System.Timers.ElapsedEventHandler(TimersTimer_Elapsed);
 
-            #endregion "Write"
+            int time_needed_to_wait = 6000 * queue_files.Count + 100;
 
-            // 判斷是否最後一行了?
-            if ((current_line == total_lines) && (current_page == total_pages))
+            // initialization
+            current_line = 0;
+            current_line = queue_files.Dequeue();
+
+            this._timer1.Start();
+
+            // wait until finished.
+            System.Threading.Thread.Sleep(time_needed_to_wait);
+
+            #endregion download files
+
+            #region last page?
+
+            // 判斷是否最後一頁了?
+            if (current_page == total_pages)
             {
-                // 已讀完所有頁面及所有行數
+                // 已讀完所有頁面
                 // 結束
                 return;
             }
-            else if (current_line == total_lines)
+            else
             {
                 // 如果還有下一頁, 前往下一頁
                 current_page++;
@@ -263,36 +271,15 @@ namespace NHIVPNc
                         a.click();
                     }
                 }
+                return;
             }
-            else
-            {
-                // 還有下一行, 此時有兩種情形, 第一種該存檔, 第二種不用存檔
-                // to do
-                current_line++;
-                if (should_click)
-                {
-                    //vpnweb.LoadCompleted += Vpn_Line_LoadCompleted;
-                    IHTMLElement a = tr.children[4].children[0];
-                    a.click();
-                    System.Threading.Thread.Sleep(1000);
-                    Debug.WriteLine($"button click: {a.outerHTML}");
-                }
-                else
-                {
-                    // 下一行
-                    Vpn_LineData();
-                }
-            }
+
+            #endregion last page?
         }
 
         private void Vpn_Page_LoadCompleted(object sender, NavigationEventArgs e)
         {
             Vpn_PageData();
-        }
-
-        private void Vpn_Line_LoadCompleted(object sender, NavigationEventArgs e)
-        {
-            Vpn_LineData();
         }
 
         private void Work_todo()
@@ -312,24 +299,6 @@ namespace NHIVPNc
 
         private void SP_Click(object sender, RoutedEventArgs e)
         {
-            this._timer1 = new System.Timers.Timer
-            {
-                Interval = 6000
-            };
-            this._timer1.Elapsed += new System.Timers.ElapsedEventHandler(TimersTimer_Elapsed);
-
-            // initialization
-            current_line = 0;
-            queue_item.Enqueue(1);
-            queue_item.Enqueue(3);
-            queue_item.Enqueue(5);
-            queue_item.Enqueue(7);
-            queue_item.Enqueue(9);
-            current_line = queue_item.Dequeue();
-
-            this._timer1.Start();
-
-
         }
 
         private void TimersTimer_Elapsed(object sender, ElapsedEventArgs e)
@@ -353,17 +322,16 @@ namespace NHIVPNc
                 };
                 thr.Start();
 
-                if (queue_item.Count == 0)
+                if (queue_files.Count == 0)
                 {
                     this._timer1.Stop();
                 }
                 else
                 {
-                    current_line = queue_item.Dequeue();
+                    current_line = queue_files.Dequeue();
                 }
             }));
         }
-
 
         #endregion "Buttons"
     }
